1cb12b62b1138da7b65e578b79429a75a81bdb65
Christian Wenz <christian@wenz.org>
2011-12-09 18:47:26 +0100

    initial commit

---
+++ AutoComplete.Service.asmx
@@ -0,0 +1,25 @@
+﻿<%@ WebService Language="C#" Class="AutoComplete" %>
+
+using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[System.Web.Script.Services.ScriptService]
+public class AutoComplete  : System.Web.Services.WebService {
+
+    [WebMethod]
+    public string[] autoComplete(string prefixText, int count)
+    {
+        return new string[] {
+                prefixText + " 1",
+                prefixText + " 2",
+                prefixText + " 3",
+                prefixText + " 4",
+                prefixText + " 5"
+            };
+    }
+    
+}
---
+++ jquery.act.autocomplete.js
@@ -0,0 +1,44 @@
+﻿/*
+* Ajax Control Toolkit Autocomplete plug-in 0.1.0
+*
+* Copyright (c) 2011 Christian Wenz
+*
+* Dual licensed under the MIT and GPL licenses:
+*   http://www.opensource.org/licenses/mit-license.php
+*   http://www.gnu.org/licenses/gpl.html
+*/
+
+(function ($) {
+    $.fn.actAutocomplete = function (options) {
+        var settings = {
+            // maximum number of elements to return from the service
+            count: 10
+        };
+        if (options) {
+            $.extend(settings, options);
+        }
+
+        return this.each(function () {
+            if (settings.servicePath && settings.serviceMethod) {
+                $(this).autocomplete({
+                    source: function (request, response) {
+                        var term = request.term;
+                        $.ajaxSettings.contentType = "application/json";
+                        $.ajax({
+                            type: "POST",
+                            url: settings.servicePath + "/" + settings.serviceMethod,
+                            data: JSON.stringify({ prefixText: term, count: settings.count }),
+                            format: "json",
+                            contentType: "application/json; charset=utf-8",
+                            success: function (result) {
+                                response.apply(this, [result.d]);
+                            }
+                        });
+                    }
+                });
+            } else {
+                throw "You need to set the servicePath and serviceMethod options!";
+            }
+        });
+    }
+})(jQuery);
